ee064aff173f974229ec4d3984997ac6cd7716ad
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-01 00:43:45 -0300

    :bug: Fix line fill function

--- lua/utils/text_manipulation.lua
+++ lua/utils/text_manipulation.lua
@@ -23,21 +23,19 @@ function M.fill_with_pattern(pattern)
   local textwidth = vim.o.textwidth
 
   -- Get the character index at the cursor position.
-  local char_id = utf8_aux.get_char_index_at_bytepos(line[1], cur_pos[2]-1)
+  local char_id = utf8_aux.get_char_index_at_bytepos(line[1], cur_pos[2] + 1)
 
   -- Get the display size of the line in the selected character.
   local subline_dlen = utf8_aux.display_len(utf8.sub(line[1], 1, char_id))
 
   -- Count how many character we must add from the cursor position until the
   -- break column.
-  local num_rep = math.floor((textwidth - subline_dlen)/pattern_len) - 1
+  local num_rep = math.floor((textwidth - subline_dlen)/pattern_len)
 
   -- If the number of repetitions is equal or less than 0, then it means that we
   -- have no space between the cursor and the column defined by `textwidth`. In
   -- this case, we should do nothing.
   if num_rep >= 0 then
-    -- Fill the line with the character `char`.
-    local char_id = utf8_aux.get_char_index_at_bytepos(line[1], cur_pos[2])
     local output = utf8.sub(line[1], 1, char_id) .. string.rep(pattern, num_rep)
 
     -- Complete the fill with part of the pattern.
@@ -50,7 +48,7 @@ function M.fill_with_pattern(pattern)
         local w = wcwidth(rune)
         if w < 0 then break end
         inc = inc + w
-        if rem <= inc then break end
+        if inc > rem then break end
         output = output .. utf8.char(rune)
       end
     end
