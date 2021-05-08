24ded519a9343861ed622283352badb2317fbf86
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-08 15:16:49 -0300

    :wrench: Add option for BlueStyle in Julia

--- lua/config/julia/init.lua
+++ lua/config/julia/init.lua
@@ -3,3 +3,8 @@
 
 -- Substitute LaTeX symbols after typing.
 vim.g.latex_to_unicode_auto = true
+
+-- Alignment configuration.
+vim.g.julia_indent_align_brackets = 0
+vim.g.julia_indent_align_funcargs = 0
+vim.g.julia_indent_align_import = 0
