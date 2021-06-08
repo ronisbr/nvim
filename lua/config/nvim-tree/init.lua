624561506ad7a3dc51dcc192d4da05dcdf1e50ca
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-06-08 11:34:54 -0300

    :wrench: Update options of nvim-tree

--- lua/config/nvim-tree/init.lua
+++ lua/config/nvim-tree/init.lua
@@ -2,7 +2,8 @@
 -- ============================================================================
 
 -- Do not interfere with netrw.
+vim.g.nvim_tree_auto_close = true
 vim.g.nvim_tree_disable_netrw = false
 vim.g.nvim_tree_hijack_netrw = false
-vim.g.nvim_tree_auto_close = true
+vim.g.nvim_tree_update_cwd = false
 vim.g.nvim_tree_width_allow_resize = true
