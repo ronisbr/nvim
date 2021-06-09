1650cc10788622393cf340ce1d6dad4702767560
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-06-09 15:12:55 -0300

    :wrench: Unpin nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function()
   use 'honza/vim-snippets'
   use 'hrsh7th/nvim-compe'
   use 'junegunn/vim-easy-align'
+  use 'kyazdani42/nvim-tree.lua'
   use 'kyazdani42/nvim-web-devicons'
   use 'lervag/vimtex'
   use 'lewis6991/gitsigns.nvim'
@@ -43,11 +44,6 @@ return require('packer').startup(function()
   use {
     'lukas-reineke/indent-blankline.nvim',
     branch = 'lua'
-  }
-
-  use {
-    'kyazdani42/nvim-tree.lua',
-    commit = '3196205131a2d4d53d92d195215ee72343349e5b'
   }
 
 end)
