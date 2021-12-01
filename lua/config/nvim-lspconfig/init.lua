9e1eb8881f3b414872c953ebf85bf281a6b4da32
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-12-01 11:08:47 -0300

    :wrench: Fix some problems in Julia LSP

--- lua/config/nvim-lspconfig/init.lua
+++ lua/config/nvim-lspconfig/init.lua
@@ -2,6 +2,7 @@
 -- ============================================================================
 
 local nvim_lsp = require('lspconfig')
+local util = require('lspconfig/util')
 
 -- Do not show virtual text since we are using lspsaga.
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
@@ -15,13 +16,23 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 -- =============================================================================
 
 -- To use Julia LSP, execute the following command:
---     julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")
+--     julia --project=~/.julia/environments/nvim-lspconfig
+--     ]
+--     add CSTParser#master LanguageServer#master StaticLint#master SymbolServer#master
+--
+-- NOTE: The new configuration of root_dir is not compatible with single file
+-- If we use the default configuration of `root_dir`, then we have problems when
+-- running Julia LSP using single file mode (without a project).
 
-nvim_lsp['julials'].setup({ })
+nvim_lsp.julials.setup({
+  root_dir = function(fname)
+    return util.find_git_ancestor(fname) or util.path.dirname(fname)
+  end,
+})
 
 -- =============================================================================
 --                                    C++
 -- =============================================================================
 
 -- To use C++ LSP, the software `ccls` must be installed.
-nvim_lsp['ccls'].setup({ })
+nvim_lsp.ccls.setup({ })
