e6de25f403381ebd3fc3bf869a5257bd7b7ee176
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-01-13 21:54:38 -0300

    :sparkles: Add Julia vscode snippets

--- lua/config/luasnip/init.lua
+++ lua/config/luasnip/init.lua
@@ -3,6 +3,15 @@
 
 local ls = require("luasnip")
 
+-- Function to return the folder of `package.json` from the `friendly-snippets`
+-- package. This function is necessary to load the VSCode snippets from a user
+-- path together with the package `friendly-snippets`.
+local function get_snippet_rtp()
+	return vim.tbl_map(function(itm)
+		return vim.fn.fnamemodify(itm, ":h")
+	end, vim.api.nvim_get_runtime_file("package.json", true))
+end
+
 -- LuaSnip general configuration
 -- =============================================================================
 
@@ -13,4 +22,9 @@ ls.config.setup({store_selection_keys="<Tab>"})
 
 require("config.luasnip.julia").config()
 
-require("luasnip.loaders.from_vscode").load()
+require("luasnip.loaders.from_vscode").load({
+  paths = {
+    unpack(get_snippet_rtp()),
+    "~/.nvim/vscode_snippets"
+  }
+})
