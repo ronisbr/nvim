ebb246746a79e024698212a352a193b7275cd9ec
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-01-18 20:41:09 -0300

    :wrench: Remove ultisnips

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,6 @@ return require('packer').startup(function()
   use 'L3MON4D3/LuaSnip'
   use 'NTBBloodbath/galaxyline.nvim'
   use 'NTBBloodbath/doom-one.nvim'
-  use 'SirVer/ultisnips'
   use 'Th3Whit3Wolf/one-nvim'
   use 'dstein64/nvim-scrollview'
   use 'folke/todo-comments.nvim'
