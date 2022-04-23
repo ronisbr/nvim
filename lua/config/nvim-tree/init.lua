b2df35b52551a578cd22c2a7b185e63214d1799b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-04-23 13:35:01 -0300

    :wrench: Update nvim-tree

--- lua/config/nvim-tree/init.lua
+++ lua/config/nvim-tree/init.lua
@@ -4,18 +4,4 @@
 require('nvim-tree').setup({
   disable_netrw = false,
   hijack_netrw  = false,
-  auto_close    = true,
-  update_cwd    = false,
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint    = "",
-      info    = "",
-      warning = "",
-      error   = "",
-    },
-  },
-  view = {
-    auto_resize = true,
-  },
 })
