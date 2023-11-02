8719ec6e9931fa71cf6f82f9bc539f61e1c15696
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-11-02 12:17:13 -0300

    :wrench: Improve noice configuration

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,12 +4,43 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      cmdline = {
+        format = {
+          search_down = { kind = "search", pattern = "^/",  icon = " ", lang = "regex" },
+          search_up   = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+        },
+      },
+
+      commands = {
+        last = {
+          opts = {
+            -- Add new line between the header and the message.
+            format = {
+              "{level} ",
+              "{date} ",
+              "{event}",
+              { "{kind}", before = { ".", hl_group = "NoiceFormatKind" } },
+              " ",
+              "{title} ",
+              "{cmdline}\n\n",
+              "{message}",
+            },
+            win_options = {
+              winhighlight = {
+                Normal = "Normal",
+                FloatBorder = "Normal"
+              },
+            },
+          },
+        },
+      },
+
       views = {
         mini = {
           -- We will not use the status bar. Thus, let's move this view to the very bottom.
           position = { row = "100%", col = "100%" }
         }
-      }
+      },
     }
   }
 }
