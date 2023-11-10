b2a55c0d8483c4c2a03d33edeaaa7efe0942c934
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-11-09 22:15:12 -0300

    :bug: Do not enable spell by default
    By enabling spell, the startup time increased by 80%.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,7 +8,6 @@ opt.guifont = "JetBrains Mono NL,Apple Color Emoji,Symbols Nerd Font Mono:h13"
 opt.pumblend = 0
 opt.shiftwidth = 4
 opt.showtabline = 0
-opt.spell = true
 opt.spelllang = "en_us,pt_br"
 opt.spellsuggest = "best,7"
 opt.tabstop = 4
