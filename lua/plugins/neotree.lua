a1661dd81c026183e5f3ee5114efdfe4cc83842b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-11-12 19:39:41 -0300

    :wrench: Improve netrw hijack by neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,5 +4,10 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   dependencies = {
     "s1n7ax/nvim-window-picker"
+  },
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = "open_current"
+    }
   }
 }
