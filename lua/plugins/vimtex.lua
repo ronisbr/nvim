c5be23e90a5f0c5b0b437256d67b215d0fb3846f
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2023-11-29 16:06:53 -0300

    :wrench: Configure Skim as PDF viewer in VimTex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,13 @@
 return {
   {
     "lervag/vimtex",
-    ft = "tex",
-    lazy = true,
+    init = function()
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_skim_sync = 1
+      vim.g.vimtex_compiler_latexmk = {
+        continuous = 0
+      }
+    end,
+    lazy = false,
   }
 }
