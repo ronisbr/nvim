71f2c32fd0b12441968caf7ef002c110816e1446
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-05-01 20:35:56 -0300

    :wrench: Remove mini-comment
    The functionality is now builtin neovim 0.10.

---
+++ lua/plugins/mini-comment.lua
@@ -0,0 +1,7 @@
+-- Plugin configuration: mini-indentscope --------------------------------------------------
+
+-- Disable `mini.comment` since the functionality is now in Neovim 0.10.
+return {
+  "mini.comment",
+  enabled = false
+}
