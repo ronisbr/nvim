aeb5fd6a72329443603f2794f0ba0c5c429fe55f
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-05-18 11:10:39 -0300

    :bug: Fix luasnip loading

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,19 +1,17 @@
 -- Plugin configuration: LuaSnip -----------------------------------------------------------
 
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = false,
-    config = function ()
-      require("snippets.luasnip.julia").config()
-      require("luasnip").config.setup({
-        enable_autosnippets = true,
-        store_selection_keys = "<Tab>",
-        update_events = {"TextChanged", "TextChangedI"}
-      })
-      require("luasnip.loaders.from_lua").load({
-        paths = {"~/.config/nvim/snippets/"}
-      })
-    end
-  }
+  "L3MON4D3/LuaSnip",
+  lazy = false,
+  opts = {
+    enable_autosnippets = true,
+    store_selection_keys = "<Tab>",
+  },
+  config = function(_, opts)
+    require("luasnip").setup(opts)
+    require("snippets.luasnip.julia").config()
+    require("luasnip.loaders.from_lua").load({
+      paths = {"~/.config/nvim/snippets/"}
+    })
+  end
 }
