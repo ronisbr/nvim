714db2fe8ec12ae7c1bf10da580b22cde0f3ae1c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-05-18 11:11:06 -0300

    :wrench: Configure inlay hints

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,39 @@
 -- Plugin configuration: nvim-lspconfig ----------------------------------------------------
 
 return {
-  "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      servers = {
+        julials = {
+          settings = {
+            julia = {
+              inlayHints = {
+                static = {
+                  enabled = true,
+                  variableTypes = {
+                    enabled = false
+                  },
+                  parameterNames = "all"
+                }
+              }
+            }
+          }
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = {
+                enable = true,
+                paramName = "Enable"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
