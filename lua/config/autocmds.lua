33186d52f85c0e09a32768424fd86e1cf7dfdc86
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-03 20:50:55 -0300

    :wrench: Add autocmd to close some buffers with `q`

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,6 +24,30 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
+-- Close some buffers with specific filetypes using `q`.
+-- This autocmd was copied from LazyVim.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("ronisbr_close_with_q", { clear = true }),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
 -- Syntax ----------------------------------------------------------------------------------
 
 -- Markdown --
