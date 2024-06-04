e3a46d581493a8c0225eb302d97ba6475d2dec2c
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2024-06-04 16:36:37 -0300

    :wrench: Add conf for JSON, Lua, and TeX files

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,9 @@
+-- Description -----------------------------------------------------------------------------
+--
+-- Configurations realted to TeX files (*.tex).
+--
+-- -----------------------------------------------------------------------------------------
+
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.conceallevel = 0
