3754cc12284acb9d4a8c67f5593c0aff1f7ec515
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2024-06-05 15:15:24 -0300

    :art: Add new hihglight groups

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,58 @@ vim.g.maplocalleader = '\\'
 
 require("config.options")
 
+-- We must set this autocmd here to ensure it is created before the theme definition.
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "nano-theme",
+  callback = function ()
+    local c = require("nano-theme.colors").get()
+
+    -- Set additional highlight groups.
+    vim.api.nvim_set_hl(
+      0,
+      "@assignment",
+      { fg = c.nano_salient_color, bold = true }
+    )
+
+    vim.api.nvim_set_hl(
+      0,
+      "@function.macro.call",
+      { fg = c.nano_salient_color, bold = true }
+    )
+
+    vim.api.nvim_set_hl(
+      0,
+      "@markup.strong.markdown_inline",
+      {
+        bold = true,
+        fg = c.fg
+      }
+    )
+
+    vim.api.nvim_set_hl(
+      0,
+      "@string.interpolation",
+      { fg = c.nano_salient_color, bold = true }
+    )
+
+    vim.api.nvim_set_hl(
+      0,
+      "@text.literal.block.markdown",
+      { bg = c.nano_highlight_color }
+    )
+
+    vim.api.nvim_set_hl(
+      0,
+      "@text.literal.heading.markdown",
+      {
+        bg = c.nano_subtle_color,
+        fg = c.nano_strong_color,
+        bold = true,
+      }
+    )
+  end
+})
+
 require("lazy").setup(
   "plugins",
   {
