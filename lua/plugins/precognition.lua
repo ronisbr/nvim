29a9f33b5e9580804328b8e951076e96944f685e
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-09 21:05:39 -0300

    :sparkles: Add precognition.nvim

---
+++ lua/plugins/precognition.lua
@@ -0,0 +1,15 @@
+-- Description -----------------------------------------------------------------------------
+--
+-- Configuration of plugin precognition.nvim.
+--
+-- -----------------------------------------------------------------------------------------
+
+return {
+  "tris203/precognition.nvim",
+  cmd = "Precognition",
+
+  opts = {
+    startVisible = false,
+    showBlankVirtLine = false,
+  },
+}
