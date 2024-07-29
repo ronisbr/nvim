8ebe6a688b4ae450e9cb2d0221461b4c78c21b02
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-07-29 20:09:19 -0300

    :sparkles: Automatically switch the background in macOS

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,8 +10,27 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.o.background = "light"
       vim.cmd.colorscheme("nano-theme")
+
+      if vim.fn.has("macunix") then
+        local current_theme
+        current_theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
+        current_theme = current_theme:gsub("\n", "")
+        vim.o.background = (current_theme == "Dark") and "dark" or "light"
+
+        -- Every 10s we check for the current theme in macOS and change the background color
+        -- accordingly.
+        local timer = vim.uv.new_timer()
+        timer:start(10000, 10000, vim.schedule_wrap(
+          function()
+            current_theme = vim.fn.system("defaults read -g AppleInterfaceStyle")
+            current_theme = current_theme:gsub("\n", "")
+            vim.o.background = (current_theme == "Dark") and "dark" or "light"
+          end
+        ))
+      else
+        vim.o.background = "light"
+      end
     end
   }
 }
