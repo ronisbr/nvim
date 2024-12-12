ef809209eb57a193b6309f19eecb91687bf72e6c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-12-12 19:04:55 -0300

    :wrench: Disable Neogit to test LazyGit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,34 +4,36 @@
 --
 -- -----------------------------------------------------------------------------------------
 
-return {
-  {
-    "NeogitOrg/neogit",
-    cmd = "Neogit",
-    version = false,
+return { }
 
-    dependencies = {
-      "ibhagwan/fzf-lua",
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-    },
-
-    keys = {
-      { "<Leader>og", ":Neogit<CR>", "Open Neogit", silent = true }
-    },
-
-    opts = {
-      disable_insert_on_commit = false,
-      commit_editor = {
-        kind = "split",
-        staged_diff_split_kind = "vsplit"
-      },
-      log_view = {
-        kind = "vsplit"
-      },
-      kind = "replace"
-    }
-  }
-}
+-- return {
+--   {
+--     "NeogitOrg/neogit",
+--     cmd = "Neogit",
+--     version = false,
+--
+--     dependencies = {
+--       "ibhagwan/fzf-lua",
+--       "nvim-lua/plenary.nvim",
+--       "sindrets/diffview.nvim",
+--     },
+--
+--     keys = {
+--       { "<Leader>og", ":Neogit<CR>", "Open Neogit", silent = true }
+--     },
+--
+--     opts = {
+--       disable_insert_on_commit = false,
+--       commit_editor = {
+--         kind = "split",
+--         staged_diff_split_kind = "vsplit"
+--       },
+--       log_view = {
+--         kind = "vsplit"
+--       },
+--       kind = "replace"
+--     }
+--   }
+-- }
 
 -- vim:ts=2:sts=2:sw=2:et
