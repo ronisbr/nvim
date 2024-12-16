edbecfdc1c5ef646cf3d8e7591313e308b9a381d
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-12-16 13:47:22 -0300

    :bug: Fix autocomplete again

--- lua/plugins/julia.lua
+++ lua/plugins/julia.lua
@@ -11,7 +11,15 @@ return {
     version = false,
 
     config = function()
+      vim.g.latex_to_unicode_tab = false
       vim.g.latex_to_unicode_auto = true
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "julia",
+        callback = function()
+          vim.cmd("setlocal completefunc=LaTeXtoUnicode#completefunc")
+        end
+      })
     end
   }
 }
