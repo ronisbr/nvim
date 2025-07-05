c9d941aeed22b6cd63293ede42082c9d4c16c629
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-07-05 10:07:24 -0300

    :wrench: Add LSP progress indication using snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -450,20 +450,65 @@ return {
 
         -- Additional Configuraton -----------------------------------------------------------
 
-        -- Use vim.notify to show LSP progress.
-        -- vim.api.nvim_create_autocmd("LspProgress", {
-        --   callback = function(ev)
-        --     local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-        --     vim.notify(vim.lsp.status(), "info", {
-        --       id = "lsp_progress",
-        --       title = "LSP Progress",
-        --       opts = function(notif)
-        --         notif.icon = ev.data.params.value.kind == "end" and " "
-        --         or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-        --       end,
-        --     })
-        --   end,
-        -- })
+        -- Show LSP progress using notifications.
+        local progress = vim.defaulttable()
+
+        vim.api.nvim_create_autocmd(
+          "LspProgress",
+          {
+            callback = function(ev)
+              local client = vim.lsp.get_client_by_id(ev.data.client_id)
+              local value = ev.data.params.value
+
+              if not client or type(value) ~= "table" then
+                return
+              end
+
+              local p = progress[client.id]
+
+              for i = 1, #p + 1 do
+                if i == #p + 1 or p[i].token == ev.data.params.token then
+                  p[i] = {
+                    token = ev.data.params.token,
+                    msg = ("[%3d%%] %s%s"):format(
+                      value.kind == "end" and 100 or value.percentage or 100,
+                      value.title or "",
+                      value.message and (" **%s**"):format(value.message) or ""
+                    ),
+                    done = value.kind == "end",
+                  }
+                  break
+                end
+              end
+
+              local msg = {}
+
+              progress[client.id] = vim.tbl_filter(
+                function(v)
+                  return table.insert(msg, v.msg) or not v.done
+                end,
+                p
+              )
+
+              local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+              vim.notify(
+                table.concat(msg, "\n"),
+                "info",
+                {
+                  id = "lsp_progress",
+                  title = client.name,
+                  opts = function(notif)
+                    if #progress[client.id] == 0 then
+                      notif.icon = " "
+                    else
+                      notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+                    end
+                  end
+                }
+              )
+            end,
+          }
+        )
 
         -- Neovide Configration ------------------------------------------------------------
 
