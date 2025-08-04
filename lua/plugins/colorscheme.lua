005a13217920980a38b66bbb0398303bf5148989
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-08-04 15:53:24 -0300

    :sparkles: Update colortheme to gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,41 +5,43 @@
 -- -----------------------------------------------------------------------------------------
 
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "auto",
+  --     })
+  --
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end
+  -- },
   {
-    "catppuccin/nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
     config = function()
-      require("catppuccin").setup({
-        flavour = "auto",
-        highlight_overrides = {
-          all = function(colors)
-            return {
-              MiniTablineCurrent = {
-                bg = colors.surface0,
-                fg = colors.text,
-                sp = colors.red,
-                style = { "bold" }
-              },
+      vim.g.gruvbox_material_background           = "medium"
+      vim.g.gruvbox_material_better_performance   = true
+      vim.g.gruvbox_material_dim_inactive_windows = true
+      vim.g.gruvbox_material_enable_bold          = true
+      vim.g.gruvbox_material_enable_italic        = true
+      vim.g.gruvbox_material_float_style          = "dim"
 
-              MiniTablineModifiedCurrent = {
-                bg = colors.surface0 ,
-                fg = colors.red,
-                style = { "bold" }
-              }
-            }
-          end
-        }
-      })
+      vim.cmd.colorscheme("gruvbox-material")
 
-      vim.cmd.colorscheme("catppuccin")
+      -- Customize Highlight Groups --------------------------------------------------------
+
+      vim.api.nvim_set_hl(0, "WinBar", { bold = false })
+      vim.api.nvim_set_hl(0, "WinBarNC", { bold = false })
     end
   },
-  {
-    "ronisbr/nano-theme.nvim",
-    lazy = false,
-    priority = 1000,
-  }
+  -- {
+  --   "ronisbr/nano-theme.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- }
 }
 
 -- vim:ts=2:sts=2:sw=2:et
