8bc8013ad696418732262cd15da6a32b99db64eb
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-08-11 11:17:24 -0300

    :wrench: Load missing plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,15 @@
+-- Description -----------------------------------------------------------------------------
+--
+-- Configuration for plugins.
+--
+-- -----------------------------------------------------------------------------------------
+
 require("plugins.colorscheme")
 
+-- We must load copilot **before** mini.completion becuase it overrides the default mapping
+-- for <Tab> and <S-Tab>.
 require("plugins.copilot")
+
 require("plugins.mini")
 require("plugins.snacks")
 
@@ -8,3 +17,5 @@ require("plugins.emojify")
 require("plugins.julia")
 require("plugins.lsp")
 require("plugins.treesitter")
+require("plugins.vimtex")
+require("plugins.undotree")
