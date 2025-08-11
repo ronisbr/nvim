d1d5420806d895648ac404357fe794dfc6f9a190
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-08-11 11:19:25 -0300

    :wrench: Disable emojify

--- lua/plugins/emojify.lua
+++ lua/plugins/emojify.lua
@@ -4,13 +4,13 @@
 --
 -- -----------------------------------------------------------------------------------------
 
-MiniDeps.later(
-  function()
-    MiniDeps.add({ source = "ronisbr/emojify.nvim" })
-    require("emojify").setup({ inlay = true })
-    vim.cmd("Emojify")
-  end
-)
+-- MiniDeps.later(
+--   function()
+--     MiniDeps.add({ source = "ronisbr/emojify.nvim" })
+--     require("emojify").setup({ inlay = true })
+--     vim.cmd("Emojify")
+--   end
+-- )
 
 -- vim:ts=2:sts=2:sw=2:et
 
