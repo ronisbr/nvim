b0fd7561325e4d70ab086e626072ed445fc22ae1
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-08-19 09:25:16 -0300

    :wrench: Add more LSP keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,51 +31,53 @@ MiniDeps.now(
           if
             path ~= vim.fn.stdpath('config')
             and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-            then
-              return
-            end
+          then
+            return
           end
+        end
 
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              version = 'LuaJIT',
-              path = {
-                'lua/?.lua',
-                'lua/?/init.lua',
-              },
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT',
+            path = {
+              'lua/?.lua',
+              'lua/?/init.lua',
             },
+          },
 
-            workspace = {
-              checkThirdParty = false,
-              library = { vim.env.VIMRUNTIME }
-            }
-          })
-        end,
-        settings = { Lua = {} }
-      })
+          workspace = {
+            checkThirdParty = false,
+            library = { vim.env.VIMRUNTIME }
+          }
+        })
+      end,
+      settings = { Lua = {} }
+    })
 
-      -- Create an autocmd to configure the buffer when the LSP attaches.
-      vim.api.nvim_create_autocmd(
-        "LspAttach",
-        {
-          group = vim.api.nvim_create_augroup("ronisbr-lsp-attach", { clear = true }),
-          callback = function(event)
-            local function nmap(lhs, rhs, desc)
-              vim.keymap.set("n", lhs, rhs, { buffer = event.buf, desc = desc })
-            end
-
-            nmap("gra", vim.lsp.buf.code_action, "Code Actions")
-            nmap("gri", vim.lsp.buf.implementation, "Implementation")
-            nmap("grn", vim.lsp.buf.rename, "Rename")
-            nmap("grr", function() MiniExtra.pickers.lsp({ scope = "references" }) end, "References")
+    -- Create an autocmd to configure the buffer when the LSP attaches.
+    vim.api.nvim_create_autocmd(
+      "LspAttach",
+      {
+        group = vim.api.nvim_create_augroup("ronisbr-lsp-attach", { clear = true }),
+        callback = function(event)
+          local function nmap(lhs, rhs, desc)
+            vim.keymap.set("n", lhs, rhs, { buffer = event.buf, desc = desc })
           end
-        }
-      )
 
-      -- Enable LSP Clients ----------------------------------------------------------------
+          nmap("gra", vim.lsp.buf.code_action, "Code Actions")
+          nmap("grd", vim.lsp.buf.definition, "Definition")
+          nmap("grD", vim.lsp.buf.declaration, "Declaration")
+          nmap("gri", vim.lsp.buf.implementation, "Implementation")
+          nmap("grn", vim.lsp.buf.rename, "Rename")
+          nmap("grr", function() MiniExtra.pickers.lsp({ scope = "references" }) end, "References")
+        end
+      }
+    )
 
-      vim.lsp.enable("julials")
-      vim.lsp.enable("lua_ls")
+    -- Enable LSP Clients ----------------------------------------------------------------
+
+    vim.lsp.enable("julials")
+    vim.lsp.enable("lua_ls")
   end
 )
 
