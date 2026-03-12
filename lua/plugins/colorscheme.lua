756a358334be5ce3daeeb829d37892b391366fe2
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-03-12 20:37:21 -0300

    :lipstick: Switch colorscheme to kanagawa-dragon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,17 +6,16 @@
 
 MiniDeps.now(
   function()
-    MiniDeps.add({ source = "sainnhe/gruvbox-material", })
+    -- MiniDeps.add({ source = "sainnhe/gruvbox-material", })
 
-    vim.g.gruvbox_material_background           = "medium"
-    vim.g.gruvbox_material_better_performance   = true
-    vim.g.gruvbox_material_dim_inactive_windows = true
-    vim.g.gruvbox_material_enable_bold          = true
-    vim.g.gruvbox_material_enable_italic        = true
-    vim.g.gruvbox_material_float_style          = "dim"
+    -- vim.g.gruvbox_material_background           = "medium"
+    -- vim.g.gruvbox_material_better_performance   = true
+    -- vim.g.gruvbox_material_dim_inactive_windows = true
+    -- vim.g.gruvbox_material_enable_bold          = true
+    -- vim.g.gruvbox_material_enable_italic        = true
+    -- vim.g.gruvbox_material_float_style          = "dim"
 
-
-    vim.cmd.colorscheme("gruvbox-material")
+    -- vim.cmd.colorscheme("gruvbox-material")
 
     -- MiniDeps.add({ source = "ronisbr/nano-theme.nvim" })
     --
@@ -27,6 +26,10 @@ MiniDeps.now(
 
     -- vim.api.nvim_set_hl(0, "WinBar", { bold = false })
     -- vim.api.nvim_set_hl(0, "WinBarNC", { bold = false })
+
+    MiniDeps.add({ source = "rebelot/kanagawa.nvim" })
+    require("kanagawa").setup({})
+    vim.cmd.colorscheme("kanagawa-dragon")
   end
 )
 
