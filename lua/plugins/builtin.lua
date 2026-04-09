255d7118a67eaec8b54205a805eda0a9b5f5d509
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-04-09 10:24:54 -0300

    :wrench: Put the msg dialog in the top right corner

--- lua/plugins/builtin.lua
+++ lua/plugins/builtin.lua
@@ -6,9 +6,12 @@
 
 -- UI2 -------------------------------------------------------------------------------------
 
+local ui2  = require("vim._core.ui2")
+local msgs = require("vim._core.ui2.messages")
+
 -- General configuraton for UI2.
 vim.opt.cmdheight = 0
-require('vim._core.ui2').enable({
+ui2.enable({
   enable = true,
   msg = {
     targets = {
@@ -98,6 +101,26 @@ vim.api.nvim_create_autocmd(
     end,
   }
 )
+
+-- Override the default position of messages to be at the top right corner of the editor.
+local orig_set_pos = msgs.set_pos
+msgs.set_pos = function(tgt)
+  orig_set_pos(tgt)
+
+  if (tgt == "msg" or tgt == nil) and vim.api.nvim_win_is_valid(ui2.wins.msg) then
+    pcall(
+      vim.api.nvim_win_set_config,
+      ui2.wins.msg,
+      {
+        relative = "editor",
+        anchor   = "NE",
+        row      = 0,
+        col      = vim.o.columns - 1,
+        border   = "rounded",
+      }
+    )
+  end
+end
 
 -- undotree --------------------------------------------------------------------------------
 
