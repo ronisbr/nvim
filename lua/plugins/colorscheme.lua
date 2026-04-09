4f0aa3bc8dfc10f0a2561b5ab7d8d6361469bd63
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-04-09 10:25:04 -0300

    :wrench: Set default nano-theme dark variant

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,13 @@
 
 MiniMisc.now(
   function()
+    require("nano-theme").setup({
+      light_variant = "default",
+      dark_variant  = "ink",
+    })
+
     vim.cmd.colorscheme("nano-theme")
+
     require("auto-dark-mode").setup()
   end
 )
