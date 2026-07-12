81bf41893be903eebe292f97bfb83ba7ff87e469
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-07-12 20:51:50 -0300

    :wrench: Improve markdown configuration

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,10 +4,10 @@
 --
 -- -----------------------------------------------------------------------------------------
 
-vim.opt_local.linebreak = true
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.wrap = true
 vim.opt_local.breakindent = true
 vim.opt_local.colorcolumn = ""
+vim.opt_local.linebreak = true
+vim.opt_local.shiftwidth = 2
 vim.opt_local.showbreak = "↳ "
+vim.opt_local.tabstop = 2
+vim.opt_local.wrap = true
