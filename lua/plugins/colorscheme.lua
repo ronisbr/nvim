1b95a78b3cdd4ff599e173638a5bfed7f611a240
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-07-24 17:01:18 -0300

    :wrench: Use wood variant of nano-theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,12 +7,15 @@
 MiniMisc.now(
   function()
     require("nano-theme").setup({
-      light_variant = "gilded",
-      dark_variant  = "gilded",
+      light_variant      = "gilded",
+      dark_variant       = "wood",
+      transparent        = true,
+      transparent_floats = false,
+      float_blend        = 5,
     })
 
     vim.cmd.colorscheme("nano-theme")
 
-    require("auto-dark-mode").setup()
+    -- require("auto-dark-mode").setup()
   end
 )
